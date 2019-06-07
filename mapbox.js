aabbd8c4c95aafd86f8e9724e71153da119ac948
csorge-uw <46582598+csorge-uw@users.noreply.github.com>
2019-06-07 03:04:22 -0700

    Update mapbox.js

--- mapbox.js
+++ mapbox.js
@@ -77,7 +77,7 @@ map.on('load', function() {
         "paint": {
             'circle-radius': [
                 'interpolate', ['linear'], ['zoom'],
-                10, ['/', ['get', 'total_int'], 200],
+                10, ['/', ['get', 'total_int'], 250],
                 16, ['/', ['get', 'total_int'], 100],
             ],
             "circle-color": "#ff0000",
